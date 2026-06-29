5efdffb817c78b1c7d5675685f6c2e251c17494e
GJC <gmgjcqwe@gmail.com>
2026-06-29 21:54:32 +0800

    feat: 将API Key迁移到后端dotenv环境变量，提升安全性；新增docs/mockups设计稿

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+require('dotenv').config()
 const schoolsRouter = require('./routes/schools')
 const majorsRouter = require('./routes/majors')
 const provincesRouter = require('./routes/provinces')
